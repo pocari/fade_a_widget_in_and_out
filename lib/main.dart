d56d4068cf847bade20bc4315cc6b6d9694074d4
pocari <caffelattenonsugar@gmail.com>
2019-11-24 09:51:35 +0900

    Fade the box in and out

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,48 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: "Opacity Demo",
+        home: MyHomePage(title: "opacity demo"),
+      );
+}
+
+class MyHomePage extends StatefulWidget {
+  final String title;
+
+  MyHomePage({Key key, @required this.title}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _visible = true;
+  @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+          child: AnimatedOpacity(
+        opacity: _visible ? 1.0 : 0.0,
+        duration: Duration(milliseconds: 500),
+        child: Container(
+          width: 200.0,
+          height: 200.0,
+          color: Colors.green,
+        ),
+      )),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            _visible = !_visible;
+          });
+        },
+        tooltip: "Toggle Opacity",
+        child: Icon(Icons.flip),
+      ),
+    );
   }
 }
